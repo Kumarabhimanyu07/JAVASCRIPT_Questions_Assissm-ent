f2b7cb127609409cc59839d3518dc0acfef3fed5
Kumarabhimanyu07 <121421368+Kumarabhimanyu07@users.noreply.github.com>
2024-04-04 13:00:41 +0530

    Delete Password validator directory

--- Password validator/16. Generate a random number.js
+++
@@ -1,8 +0,0 @@
-// Self-invoking arrow function to generate a random number between 1 and 100
-const randomNum = (() => {
-    // Generate a random number between 1 and 100
-    return Math.floor(Math.random() * 100) + 1;
-})();
-
-// Output the random number
-console.log("Random number:", randomNum);
